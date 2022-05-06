407691d0799b521306943a63cef6a836dd7a3234
Lyudmila Dermenzhii <lsmolyaninova@gmail.com>
2022-05-07 01:32:42 +0300

    fix transaction

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -13,35 +13,26 @@ export function TransactionHistory({items}) {
   </thead>
 
   <tbody>
-          {items.map(({id,type,amount,currency}) =>
-            <Transaction
-              key={id}
-              type={type}
-              amount={amount}
-              currency={currency}
-            />)}    
+          {items.map(({ id, type, amount, currency }) =>
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>)}
   </tbody>
 </table>
     )
 }
-TransactionHistory.propTypes = {
-  items: PropTypes.array.isRequired
-}
 
-export function Transaction({ type, amount, currency }) {
-  return (
-    <tr>
-      <td>{type}</td>
-      <td>{amount}</td>
-      <td>{currency}</td>
-    </tr>
-  )
-}
-Transaction.propTypes = {
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
     type: PropTypes.string.isRequired,
     amount: PropTypes.string.isRequired,
     currency: PropTypes.string.isRequired,
-    id:PropTypes.string
-}  
+    id: PropTypes.number.isRequired,
+    })
+    )
+}
 
 
